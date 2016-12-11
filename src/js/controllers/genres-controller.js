7d43828806f9568357c14c6bf81c135f48aff66a
Guido Barbaglia <guido.barbaglia@gmail.com>
2016-12-11 17:41:04 +1100

    Update genres-controller.js

--- src/js/controllers/genres-controller.js
+++ src/js/controllers/genres-controller.js
@@ -5,7 +5,7 @@
     var app = angular.module('NYTBestSellers');
 
     app.controller('GenresController', function ($scope, $routeParams, $http) {
-        var url = 'http://api.nytimes.com/svc/books/v3/lists/names.json?api-key=sample-key';
+        var url = 'http://api.nytimes.com/svc/books/v3/lists/names.json?api-key=42440be70c704f3eaef8a428d47cdca4';
         $http.get(url).then(function (response) {
             $scope.genres = response.data.results;
         }).catch(function (e) {
