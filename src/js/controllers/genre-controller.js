c7e54ad266730b1d6dc7cbb533aa33a06e21c1f5
Guido Barbaglia <guido.barbaglia@gmail.com>
2016-12-11 17:39:33 +1100

    API key

--- src/js/controllers/genre-controller.js
+++ src/js/controllers/genre-controller.js
@@ -5,7 +5,7 @@
     var app = angular.module('NYTBookReviews');
 
     app.controller('GenreController', function ($scope, $routeParams, $http) {
-        var url = 'http://api.nytimes.com/svc/books/v3/lists/' + $routeParams.genre + '.json?sort-by=rank&sort-order=ASC&api-key=sample-key';
+        var url = 'http://api.nytimes.com/svc/books/v3/lists/' + $routeParams.genre + '.json?sort-by=rank&sort-order=ASC&api-key=42440be70c704f3eaef8a428d47cdca4';
         $http.get(url).then(function (response) {
             $scope.books = response.data.results.books;
             $scope.genre = $routeParams.genre;
